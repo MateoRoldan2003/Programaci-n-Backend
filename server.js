3750476ac1fe5e739b0836d2823d2f1e24d5fd93
Mateo <roldanmateo02@gmail.com>
2024-02-08 04:33:17 -0300

    Desafio

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const config = require('./config');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/ecommerce', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(config.mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({ secret: 'd1b9bda1b4e2e817c631', resave: true, saveUninitialized: true }));
+app.use(session({ secret: config.sessionSecret, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -20,8 +21,7 @@ const sessionsRoutes = require('./routes/api/sessions');
 app.use('/auth', authRoutes);
 app.use('/api/sessions', sessionsRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = config.port;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
