67f386e6474a153fba810adb9d67e845cde85f9b
Mateo <roldanmateo02@gmail.com>
2024-02-27 19:59:55 -0300

    Descripción corta de los cambios

--- config/passport-strategies.js
+++ config/passport-strategies.js
@@ -3,33 +3,17 @@ const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('./models/user');
+const config = require('../config');
 
 passport.use(new LocalStrategy({
   usernameField: 'email',
   passwordField: 'password'
 }, async (email, password, done) => {
-  try {
-    const user = await User.findOne({ email });
-    if (!user || !user.validPassword(password)) {
-      return done(null, false, { message: 'Invalid email or password' });
-    }
-    return done(null, user);
-  } catch (error) {
-    return done(error);
-  }
 }));
 
 passport.use(new JwtStrategy({
   jwtFromRequest: ExtractJwt.fromCookie(),
-  secretOrKey: 'd1b9bda1b4e2e817c631'
+  secretOrKey: config.jwtSecret
 }, async (payload, done) => {
-  try {
-    const user = await User.findById(payload.sub);
-    if (!user) {
-      return done(null, false);
-    }
-    return done(null, user);
-  } catch (error) {
-    return done(error, false);
-  }
 }));
+
